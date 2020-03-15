fa0cb9b631be183d2ab68456a04ffff3fe66b990
Jordan Fumiere <fumiere.jordan@outlook.fr>
2020-03-15 05:15:22 +0100

    Création de la page de Connexion

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		
+		<meta charset="utf-8">
+		<title>GBAF - Connexion</title>
+
+	</head>
+
+	<body>
+
+		Connexion :<br />
+		<form action="connection.php" method="post">
+
+			login <input type="text" name="login" value=""><br />
+			mot de passe <input type="password" name="pwd" value=""><br />
+			<input type="submit" name="connexion" value="Connexion">
+
+		</form>
+
+		<a href="inscription.php">S'inscrire</a>
+
+	</body>
+
+</html>
